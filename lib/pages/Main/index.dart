8e3c8b10f8631123a78350a8bbb90f4dbe0b58d6
Yui酱 <1906506454@qq.com>
2025-12-24 00:23:15 +0800

    主页tab栏的构建实现

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Cart/Cart.dart';
+import 'package:hm_shop/pages/Category/Category.dart';
+import 'package:hm_shop/pages/Home/home.dart';
+import 'package:hm_shop/pages/Mine/Mine.dart';
 
 class MainPage extends StatefulWidget {
   MainPage({Key? key}) : super(key: key);
@@ -8,12 +12,72 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  // 定义数据 根据数据进行渲染
+  final List<Map<String, String>> _tablist = [
+    {
+      "icon": "lib/assets/ic_public_home_normal.png",
+      "active_icon": "lib/assets/ic_public_home_active.png",
+      "text": "首页",
+    },
+    {
+      "icon": "lib/assets/ic_public_pro_normal.png",
+      "active_icon": "lib/assets/ic_public_pro_active.png",
+      "text": "分类",
+    },
+    {
+      "icon": "lib/assets/ic_public_cart_normal.png",
+      "active_icon": "lib/assets/ic_public_cart_active.png",
+      "text": "购物车",
+    },
+    {
+      "icon": "lib/assets/ic_public_my_normal.png",
+      "active_icon": "lib/assets/ic_public_my_active.png",
+      "text": "我的",
+    },
+  ];
+  int _currentIndex = 0;
+
+  // 返回四个分类内容以List<BottomNavigationBarItem>的类型
+  List<BottomNavigationBarItem> _getTabBarWidget() {
+    return List.generate(4, (index) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(_tablist[index]["icon"]!, width: 30, height: 30),
+        activeIcon: Image.asset(
+          _tablist[index]["active_icon"]!,
+          width: 30,
+          height: 30,
+        ),
+        label: _tablist[index]["text"],
+      );
+    });
+  }
+
+List<Widget> _getChildren(){
+  return [
+    HomeView(),
+    CategoryView(),
+    CartView(),
+    MineView()
+  ];
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("主页"),),
-      body: Center(
-        child: Text("主页"),
+      body: SafeArea(child: IndexedStack(
+        index: _currentIndex,
+        children: _getChildren(),
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _getTabBarWidget(),
+        currentIndex: _currentIndex,
+        selectedItemColor: const Color.fromARGB(255, 253, 144, 237),
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.black,
+        onTap: (int index) {
+          _currentIndex = index;
+          setState(() {});
+        },
       ),
     );
   }
