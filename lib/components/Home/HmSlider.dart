599f5ef02c961dae0a05663182ec9ed39d816354
Yui酱 <1906506454@qq.com>
2025-12-25 17:04:51 +0800

    完成轮播图的指示灯导航

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -11,6 +11,8 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  final CarouselSliderController _controller = CarouselSliderController();
+  int _currentIndex = 0;
   _getWidth() {
     final double screenWidth = (MediaQuery.of(context).size.width);
     return screenWidth;
@@ -23,6 +25,7 @@ class _HmSliderState extends State<HmSlider> {
 
   Widget _getSlider() {
     return CarouselSlider(
+      carouselController: _controller,
       items: List.generate(widget.bannerList.length, (int index) {
         return Image.network(
           widget.bannerList[index].imgUrl,
@@ -33,8 +36,73 @@ class _HmSliderState extends State<HmSlider> {
       options: CarouselOptions(
         viewportFraction: 1,
         autoPlay: true,
-        autoPlayInterval: Duration(seconds: 1),
+        autoPlayInterval: Duration(seconds: 4),
         height: _getHeigth(),
+        onPageChanged: (index, reason) {
+          _currentIndex = index;
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  Widget _getSearch() {
+    return Positioned(
+      // 💡 在 Stack 里用 Positioned 控制位置更专业
+      top: 10,
+      left: 15,
+      right: 15,
+      child: Container(
+        height: 40,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          color: Colors.black.withValues(alpha: 0.3),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        alignment: Alignment.centerLeft,
+        child: Row(
+          children: [
+            Icon(Icons.search, color: Colors.white70, size: 18),
+            SizedBox(width: 8),
+            Text("搜索...", style: TextStyle(color: Colors.white70)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (int index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.jumpToPage(index);
+                print("点击了$index");
+                setState(() {});
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds :300),
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                height: 6,
+                width: index == _currentIndex ? 40 : 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  color: index == _currentIndex
+                      ? const Color.fromARGB(255, 214, 214, 214)
+                      : const Color.fromARGB(138, 9, 9, 9),
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
@@ -47,6 +115,6 @@ class _HmSliderState extends State<HmSlider> {
     //   alignment: Alignment.center,
     //   child: Text("轮播图", style: TextStyle(color: Colors.white)),
     // );
-    return Stack(children: [_getSlider()]);
+    return Stack(children: [_getSlider(), _getSearch(), _getDots()]);
   }
 }
